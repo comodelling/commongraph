10e78698cf0c44478708736b377eb8c15f69c92d
Mario Morvan <mario.morvan@ucl.ac.uk>
2024-10-22 23:50:12 +0100

    delete, update, search

--- data_models.py
+++ data_models.py
@@ -5,10 +5,15 @@ class Node(BaseModel):
     node_type: str = "node"
     summary: str
     description: str | None = None
-    node_id: int | None = None  # only if retrieved from database
 
 
 class Edge(BaseModel):
     edge_type: str
     source_node: int
     target_node: int
+
+
+class NodeSearch(BaseModel):
+    node_type: str | None = None
+    summary: str | None = None
+    description: str | None = None
--- main.py
+++ main.py
@@ -1,14 +1,13 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+from starlette.responses import HTMLResponse
 from gremlin_python.process.graph_traversal import __
 from gremlin_python.driver.driver_remote_connection import DriverRemoteConnection
 from gremlin_python.driver.serializer import GraphSONSerializersV3d0
 from gremlin_python.process.anonymous_traversal import traversal
-from starlette.responses import HTMLResponse
 from gremlin_python.process.traversal import T
 from gremlin_python.process.traversal import Cardinality
-from gremlin_python.driver.aiohttp.transport import AiohttpTransport
 
-from data_models import Node, Edge
+from data_models import Node, Edge, NodeSearch
 from graph_utils import plot_graph
 
 
@@ -82,12 +81,12 @@ def get_edge_list():
 
 
 @app.get("/nodes/{node_id}")
-def get_node(node_id: str):
+def get_node(node_id: int):
     return g.V(node_id).next()
 
 
 @app.get("/edge/{edge_id}")
-def get_edge(edge_id: str):
+def get_edge(edge_id: int):
     return g.E(edge_id).next()
 
 
@@ -107,5 +106,47 @@ def add_edge(edge: Edge):
     return edge
 
 
-# TODO: delete and update APIs for network elements
+@app.delete("/nodes/{node_id}")
+def delete_node(node_id: int):
+    node = g.V(node_id).next()
+    if node is None:
+        raise HTTPException(status_code=404, detail="Node not found")
+    g.V(node_id).bothE().drop().iterate()
+    g.V(node_id).drop().iterate()
+    return {"message": "Node deleted successfully"}
+
+
+@app.delete("/edges/{edge_id}")
+def delete_edge(edge_id: str):
+    edge = g.E(edge_id).next()
+    if edge is None:
+        raise HTTPException(status_code=404, detail="Edge not found")
+    g.E(edge_id).drop().iterate()
+    return {"message": "Edge deleted successfully"}
+
+
+@app.put("/nodes/{node_id}")
+def update_node(node_id: str, node: Node):
+    node = g.V(node_id).nest()
+    if node is None:
+        raise HTTPException(status_code=404, detail="Node not found")
+    if node.summary is not None:
+        g.V(node_id).property("summary", node.summary).iterate()
+    if node.description is not None:
+        g.V(node_id).property("description", node.description).iterate()
+    return {"message": "Node updated successfully"}
+
+
+@app.get("/nodes/search")
+def search_nodes(node_search: NodeSearch):
+    traversal = g.V()
+    if node_search.node_type is not None:
+        traversal = traversal.has_label(node_search.node_type)
+    if node_search.summary is not None:
+        traversal = traversal.has("summary", node_search.summary)
+    if node_search.description is not None:
+        traversal = traversal.has("description", node_search.description)
+    return traversal.toList()
+
+
 # TODO: update the viz API

9aed467711f93486925c3c775812d66dc385b8b4
Mario Morvan <mario.morvan@ucl.ac.uk>
2024-11-25 16:39:40 +0000

    random proposal in side menu

--- src/components/SideMenu.vue
+++ src/components/SideMenu.vue
@@ -1,29 +1,52 @@
 <template>
-    <div class="side-menu">
-        <div class="title">Menu</div>
-        <router-link to="/">Main Page</router-link><br />
-        <router-link to="/about">About ObjectiveNet</router-link>
-    </div>
-  </template>
-  
-  <script>
-  export default {};
-  </script>
-  
-  <style scoped>
-  .side-menu {
-    width: 175px;
-    min-width: 150px;
-    height: 90vh;
-    border: 1px solid #ccc;
-    padding: 10px;
-    font-size: 12px;
-    /* margin-top: 50px; */
-    margin-left: 10px;
-    margin-top: 20px;
-  }
-  .side-menu .title {
-    font-size: 20px;
-    font-weight: bold;
+  <div class="side-menu">
+    <div class="title">Menu</div>
+    <router-link to="/">Main page</router-link><br />
+    <a href="#" @click="fetchRandomProposal">Random proposal</a><br />
+    <router-link to="/about">About ObjectiveNet</router-link>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+import { useRouter } from 'vue-router';
+
+export default {
+  setup() {
+    const router = useRouter();
+
+    const fetchRandomProposal = async () => {
+      try {
+        const response = await axios.get(`${import.meta.env.VITE_BACKEND_URL}/nodes/random`);
+        const proposal = response.data;
+        console.log('Random proposal:', proposal);
+        router.push(`/focus/${proposal.node_id}`);
+      } catch (error) {
+        console.error('Error fetching random proposal:', error);
+      }
+    };
+
+    return {
+      fetchRandomProposal,
+    };
+  },
+};
+</script>
+
+<style scoped>
+.side-menu {
+  width: 175px;
+  min-width: 150px;
+  height: 90vh;
+  border: 1px solid #ccc;
+  padding: 10px;
+  font-size: 12px;
+  /* margin-top: 50px; */
+  margin-left: 10px;
+  margin-top: 20px;
 }
-  </style>
+.side-menu .title {
+  font-size: 20px;
+  font-weight: bold;
+}
+</style>

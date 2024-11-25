5aaf044b6d7e7a165b60a46a5352b66fba88fed2
Mario Morvan <mario.morvan@ucl.ac.uk>
2024-11-25 23:32:30 +0000

    add viewport control and minimap

--- src/components/GraphRenderer.vue
+++ src/components/GraphRenderer.vue
@@ -1,9 +1,10 @@
 <script setup>
 import { ref, watch } from 'vue'
 import { VueFlow, useVueFlow } from '@vue-flow/core'
-// import { Background } from '@vue-flow/background'
-// import { ControlButton, Controls } from '@vue-flow/controls'
-// import { MiniMap } from '@vue-flow/minimap'
+import { Background } from '@vue-flow/background'
+import { ControlButton, Controls } from '@vue-flow/controls'
+import { MiniMap } from '@vue-flow/minimap'
+import Icon from './Icon.vue' // Update this line
 
 // import SpecialNode from './SpecialNode.vue'
 // import SpecialEdge from './SpecialEdge.vue'
@@ -22,6 +23,7 @@ const { onInit, setNodes, setEdges, onNodeDragStop, onConnect, addEdges, setView
 const nodes = ref([])
 const edges = ref([])
 
+const dark = ref(false)
 
 
 
@@ -45,6 +47,72 @@ watch(
   { immediate: true }
 )
 
+
+
+/**
+ * onNodeDragStop is called when a node is done being dragged
+ *
+ * Node drag events provide you with:
+ * 1. the event object
+ * 2. the nodes array (if multiple nodes are dragged)
+ * 3. the node that initiated the drag
+ * 4. any intersections with other nodes
+ */
+ onNodeDragStop(({ event, nodes, node }) => {
+  console.log('Node Drag Stop', { event, nodes, node })
+})
+
+/**
+ * onConnect is called when a new connection is created.
+ *
+ * You can add additional properties to your new edge (like a type or label) or block the creation altogether by not calling `addEdges`
+ */
+onConnect((connection) => {
+  addEdges(connection)
+})
+
+/**
+ * To update a node or multiple nodes, you can
+ * 1. Mutate the node objects *if* you're using `v-model`
+ * 2. Use the `updateNode` method (from `useVueFlow`) to update the node(s)
+ * 3. Create a new array of nodes and pass it to the `nodes` ref
+ */
+function updatePos() {
+  //TODO: fix this
+
+  console.log('Trying to ypdating Node Positions')
+  console.log('Current Nodes:', nodes)
+  const outValue =nodes.value.map((node) => {
+    return {
+      ...node,
+      position: {
+        x: Math.random() * 400,
+        y: Math.random() * 400,
+      },
+    }
+  })
+  console.log('New Nodes:', outValue)
+  setNodes(outValue)
+}
+
+/**
+ * toObject transforms your current graph data to an easily persist-able object
+ */
+function logToObject() {
+  console.log(toObject())
+}
+
+/**
+ * Resets the current viewport transformation (zoom & pan)
+ */
+function resetTransform() {
+  setViewport({ x: 0, y: 0, zoom: 1 })
+}
+
+function toggleDarkMode() {
+  dark.value = !dark.value
+}
+
 </script>
 
 <template>
@@ -56,7 +124,30 @@ watch(
       :min-zoom="0.2"
       :max-zoom="4"
     >
-    </VueFlow>
+    <Background pattern-color="#aaa" :gap="16" />
+
+    <MiniMap />
+
+    <Controls position="top-right">
+      <ControlButton title="Reset Transform" @click="resetTransform">
+        <Icon name="reset" />
+      </ControlButton>
+
+      <ControlButton title="Shuffle Node Positions" @click="updatePos">
+        <Icon name="update" />
+      </ControlButton>
+
+      <ControlButton title="Toggle Dark Mode" @click="toggleDarkMode">
+        <Icon v-if="dark" name="sun" />
+        <Icon v-else name="moon" />
+      </ControlButton>
+
+      <ControlButton title="Log `toObject`" @click="logToObject">
+        <Icon name="log" />
+      </ControlButton>
+    </Controls>
+  </VueFlow>
+
   </div>
 </template>
 

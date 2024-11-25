3ad7d9fc3182b4d6c1c50a15fab24387afd5544d
Mario Morvan <mario.morvan@ucl.ac.uk>
2024-11-25 13:57:10 +0000

    edit suggested contrib

--- src/views/About.vue
+++ src/views/About.vue
@@ -2,11 +2,13 @@
     <div class="about">
       <h1>About ObjectiveNet</h1>
       <p>ObjectiveNet aims at providing an accessible tool for people to collaboratively curate a causal network of proposals.</p>
+
+      
       <p>You can contribute by:</p>
       <ul>
         <li>Creating new proposals</li>
         <li>Enriching existing proposals</li>
-        <li>Submit issues or pull requests to: <a href="http://github.com/voicingcommons/objectivenet">http://github.com/voicingcommons/objectivenet</a></li>
+        <li>Helping to improve the code</li>
       </ul>
     </div>
   </template>

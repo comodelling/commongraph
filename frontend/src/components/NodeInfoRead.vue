0e123de745c7840d6ee206b08c287814d7db1f1a
Mario Morvan <mario.morvan@ucl.ac.uk>
2024-12-21 17:34:51 +0000

    add tooltips for node fields in read mode

--- frontend/src/components/NodeInfoRead.vue
+++ frontend/src/components/NodeInfoRead.vue
@@ -1,15 +1,19 @@
 <template>
   <div>
     <h2>{{ capitalise(node.node_type) }}</h2>
-    <strong>Title: </strong> "{{ node.title }}"<br />
+    <strong title="What is this node about?"> Title: </strong> "{{
+      node.title
+    }}"<br />
     <!-- <strong>Type:</strong> {{ capitalise(node.node_type) }}<br /> -->
-    <strong>Scope: </strong> {{ node.scope }}<br />
-    <strong>Status: </strong> {{ formatStatus(node.status) }}<br />
+    <strong title="Where/to whom does this node apply?">Scope: </strong>
+    {{ node.scope }}<br />
+    <strong title="Is this node currently live?">Status: </strong>
+    {{ formatStatus(node.status) }}<br />
     <div class="tags-container" v-if="node.tags && node.tags.length">
-      <strong>Tags: </strong>
+      <strong title="Node tags">Tags: </strong>
       <span v-for="tag in node.tags" :key="tag" class="tag">{{ tag }}</span>
     </div>
-    <strong>References: </strong> <br />
+    <strong title="A list of relevant references">References: </strong> <br />
     <ul
       class="references-list"
       v-if="node.references && node.references.length"
@@ -21,7 +25,8 @@
         {{ reference.trim() }}
       </li>
     </ul>
-    <strong>Description:</strong> <br />
+    <strong title="A longer description of the node">Description:</strong>
+    <br />
     <p>{{ node.description ? node.description : "" }}</p>
   </div>
 </template>

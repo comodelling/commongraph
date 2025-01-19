addc887f3cb51de8ffe46462aadad28349d49993
Mario Morvan <mario.morvan@ucl.ac.uk>
2025-01-19 12:32:17 +0000

    more specific default export name

--- frontend/src/components/SubnetRenderer.vue
+++ frontend/src/components/SubnetRenderer.vue
@@ -258,7 +258,22 @@ function exportSubnet() {
   const blob = new Blob([JSON.stringify(subnetData, null, 2)], {
     type: "application/json",
   });
-  saveAs(blob, "export.json");
+  const currentDate = new Date();
+  const formattedDate = `${currentDate.getFullYear()}${(
+    currentDate.getMonth() + 1
+  )
+    .toString()
+    .padStart(
+      2,
+      "0",
+    )}${currentDate.getDate().toString().padStart(2, "0")}-${currentDate
+    .getHours()
+    .toString()
+    .padStart(
+      2,
+      "0",
+    )}${currentDate.getMinutes().toString().padStart(2, "0")}${currentDate.getSeconds().toString().padStart(2, "0")}`;
+  saveAs(blob, `objnet-export-${formattedDate}.json`);
 }
 
 onNodeClick(({ node }) => {

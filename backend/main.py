1111eb72b8f04a548e7c07d0de49dce0f8bddcde
Mario Morvan <mario.morvan@ucl.ac.uk>
2025-06-14 21:15:50 +0100

    some more cleanup

--- backend/main.py
+++ backend/main.py
@@ -3,27 +3,21 @@ import logging
 import json
 import random
 
-from fastapi import Depends, FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 
-from backend.settings import settings
 from backend.version import __version__
-from backend.db.connections import get_graph_db
-from backend.models.fixed import MigrateLabelRequest
-
-from backend.db.janusgraph import JanusGraphDB
+from backend.settings import settings
 from backend.api.auth import router as auth_router
 from backend.api.users import router as users_router
 from backend.api.nodes import router as nodes_router
 from backend.api.edges import router as edges_router
 from backend.api.graph import router as graph_router
 from backend.config import (PLATFORM_NAME, NODE_TYPE_PROPS, EDGE_TYPE_PROPS, EDGE_TYPE_BETWEEN,
-                    NODE_TYPE_STYLE, EDGE_TYPE_STYLE)
+                            NODE_TYPE_STYLE, EDGE_TYPE_STYLE)
 
 logger = logging.getLogger(__name__)
 
-QUOTES_FILE = settings.QUOTES_FILE
-origins = settings.ALLOWED_ORIGINS
 
 app = FastAPI(title="CommonGraph API", version=__version__)
 
@@ -62,26 +56,9 @@ def get_config():
     }
     return {
         "platform_name": PLATFORM_NAME,
-        "node_types": node_types,  #TODO: might deprecate and use graph/schema instead
+        "node_types": node_types,
         "edge_types": edge_types,
     }
-
-
-@app.post("/migrate_label_to_property")
-def migrate_label_to_property(
-    request: MigrateLabelRequest, db: JanusGraphDB = Depends(get_graph_db)
-):
-    """Migrate the label of each vertex to a property called 'property_name'."""
-    property_name = request.property_name
-    try:
-        # Added line
-        print(f"Starting migration with property_name: {property_name}")
-        db.migrate_label_to_property(property_name)
-        print("Migration successful.")  # Added line
-        return {"message": f"Successfully migrated labels to property {property_name}"}
-    except Exception as e:
-        print(f"Migration failed with error: {e}")  # Added line
-        raise HTTPException(status_code=500, detail=str(e))
 
 
 @app.get("/quote")
@@ -90,11 +67,11 @@ def get_random_quote():
     The quotes file is specified in the QUOTES_FILE environment variable, which should be a path to a JSON file.
     The JSON file should contain a list of dictionaries, each with a "quote" key and optional "author" and "where" keys.
     """
-    if not os.path.exists(QUOTES_FILE):
+    if not os.path.exists(settings.QUOTES_FILE):
         logger.warning("Quotes file not found")
         raise HTTPException(status_code=404, detail="Quotes file not found")
 
-    with open(QUOTES_FILE, "r", encoding="utf-8") as file:
+    with open(settings.QUOTES_FILE, "r", encoding="utf-8") as file:
         try:
             quotes = json.load(file)
         except json.JSONDecodeError as e:

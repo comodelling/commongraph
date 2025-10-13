bde2f4efe51f77ef06ea9fc87a5602d2be342053
mariomorvan <mario.morvan@ucl.ac.uk>
2025-10-13 18:10:44 +0200

    add tooltips for nodes

--- frontend/src/components/graph/SpecialNode.vue
+++ frontend/src/components/graph/SpecialNode.vue
@@ -1,7 +1,8 @@
 <script setup>
 import { Handle, Position, useVueFlow } from "@vue-flow/core";
+import { computed } from "vue";
 
-defineProps({
+const props = defineProps({
   id: String,
   sourcePosition: String,
   targetPosition: String,
@@ -25,6 +26,38 @@ defineProps({
 
 defineEmits(["updateNodeInternals"]);
 
+// Create tooltip content from node data
+const tooltipContent = computed(() => {
+  if (!props.data) return '';
+  
+  const parts = [];
+  
+  // Format as: status type (scope)
+  if (props.data.status) {
+    parts.push(props.data.status);
+  }
+  
+  if (props.data.node_type) {
+    parts.push(props.data.node_type);
+  }
+  
+  if (props.data.scope) {
+    parts.push(`(${props.data.scope})`);
+  }
+  
+  let tooltip = parts.join(' ');
+  
+  // Add description on a new line if it exists
+  if (props.data.description) {
+    const desc = props.data.description.length > 100 
+      ? props.data.description.substring(0, 100) + '...' 
+      : props.data.description;
+    tooltip += tooltip ? `\n${desc}` : desc;
+  }
+  
+  return tooltip;
+});
+
 // console.log('sourcePosition', sourcePosition)
 </script>
 
@@ -35,7 +68,7 @@ defineEmits(["updateNodeInternals"]);
     title="Create implications"
   />
 
-  <span>{{ label }}</span>
+  <span :title="tooltipContent">{{ label }}</span>
 
   <Handle
     type="target"
